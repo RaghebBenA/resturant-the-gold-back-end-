8e2ea38eb916a99b493e22b3bb1fd4001ba6ac87
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-03-13 01:59:44 +0100

    basic Authentication

--- app.js
+++ app.js
@@ -34,6 +34,48 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+
+  const authHeader = req.headers.authorization;
+
+  /*Declare conditional statement to indentify if the user doesn't send authentication header
+   so we will generate an error and we 
+    will challange him to send the authenication header*/
+  if (!authHeader) {
+    const err = new Error("You are not authenticated");
+
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+  //Declare the authnication method with using that will split the Authentication header and converted to base64
+  //Second part change the base64 to string and solit it by ':'
+  const auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  //Declare the two vars username and password and assing to each one a value of the auth array index 0 and 1
+  const username = auth[0];
+  const password = auth[1];
+
+  //Declare conditional statment for the user authenication state with given userName and passWord default values
+  //Second step  if the user authentication math thoes values will move it to the next step with next() method
+  //If not we will generate error and challange the user to give the correct authentication values
+  if (username === "admin" && password === "password") {
+    next();
+  } else {
+    const err = new Error("You are not authenticated");
+
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
